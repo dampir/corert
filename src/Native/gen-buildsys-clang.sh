cfa2493f6cf983bdb69fcf799d027ef28950648b
Alexander Efremov <aedampir@gmail.com>
2017-06-19 22:16:46 +0700

    [mcs] Modified cmake files to build dynamic version of System.Native.Private.CoreLib

--- src/Native/gen-buildsys-clang.sh
+++ src/Native/gen-buildsys-clang.sh
@@ -27,6 +27,10 @@ elif command -v clang > /dev/null 2>&1
     then
         export CC="$(command -v clang)"
         export CXX="$(command -v clang++)"
+elif command -v gcc > /dev/null 2>&1
+    then
+        export CC="$(command -v gcc)"
+        export CXX="$(command -v g++)"
 else
     echo "Unable to find Clang Compiler"
     exit 1
@@ -43,58 +47,6 @@ else
         echo "Invalid Build type, only debug or release is accepted."
         exit 1
     fi
-fi
-
-OS=`uname`
-
-# Locate llvm
-# This can be a little complicated, because the common use-case of Ubuntu with
-# llvm-3.5 installed uses a rather unusual llvm installation with the version
-# number postfixed (i.e. llvm-ar-3.5), so we check for that first.
-# On FreeBSD the version number is appended without point and dash (i.e.
-# llvm-ar35).
-# Additionally, OSX doesn't use the llvm- prefix.
-if [ $OS = "Linux" -o $OS = "FreeBSD" -o $OS = "OpenBSD" -o $OS = "NetBSD" ]; then
-  llvm_prefix="llvm-"
-elif [ $OS = "Darwin" ]; then
-  llvm_prefix=""
-else
-  echo "Unable to determine build platform"
-  exit 1
-fi
-
-desired_llvm_major_version=$2
-desired_llvm_minor_version=$3
-if [ $OS = "FreeBSD" ]; then
-    desired_llvm_version="$desired_llvm_major_version$desired_llvm_minor_version"
-elif [ $OS = "OpenBSD" ]; then
-    desired_llvm_version=""
-elif [ $OS = "NetBSD" ]; then
-    desired_llvm_version=""
-else
-  desired_llvm_version="-$desired_llvm_major_version.$desired_llvm_minor_version"
-fi
-locate_llvm_exec() {
-  if command -v "$llvm_prefix$1$desired_llvm_version" > /dev/null 2>&1
-  then
-    echo "$(command -v $llvm_prefix$1$desired_llvm_version)"
-  elif command -v "$llvm_prefix$1" > /dev/null 2>&1
-  then
-    echo "$(command -v $llvm_prefix$1)"
-  else
-    exit 1
-  fi
-}
-
-llvm_ar="$(locate_llvm_exec ar)"
-[[ $? -eq 0 ]] || { echo "Unable to locate llvm-ar"; exit 1; }
-llvm_link="$(locate_llvm_exec link)"
-[[ $? -eq 0 ]] || { echo "Unable to locate llvm-link"; exit 1; }
-llvm_nm="$(locate_llvm_exec nm)"
-[[ $? -eq 0 ]] || { echo "Unable to locate llvm-nm"; exit 1; }
-if [ $OS = "Linux" -o $OS = "FreeBSD" -o $OS = "OpenBSD" -o $OS = "NetBSD" ]; then
-  llvm_objdump="$(locate_llvm_exec objdump)"
-  [[ $? -eq 0 ]] || { echo "Unable to locate llvm-objdump"; exit 1; }
 fi
 
 cmake_extra_defines=
@@ -114,11 +66,6 @@ if [[ -n "$CROSSCOMPILE" ]]; then
 fi
 
 cmake \
-  "-DCMAKE_AR=$llvm_ar" \
-  "-DCMAKE_LINKER=$llvm_link" \
-  "-DCMAKE_NM=$llvm_nm" \
-  "-DCMAKE_OBJDUMP=$llvm_objdump" \
-  "-DCMAKE_RANLIB=$llvm_ranlib" \
   "-DCMAKE_BUILD_TYPE=$build_type" \
   $cmake_extra_defines \
   "$1/src/Native"
